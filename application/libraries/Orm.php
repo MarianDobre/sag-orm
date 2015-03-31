ca1d2a58a75a4a9686c9d346ae77548ad62dbb43
Marian Dobre <marian.dobre@rinf.ro>
2015-03-31 19:17:36 +0300

    remove ORM folder

--- application/libraries/Orm.php
+++ application/libraries/Orm.php
@@ -7,13 +7,73 @@
  * @link https://github.com/maltyxx/sag-orm
  * @version 3.2.11 (20141218)
  */
-require_once(__DIR__.'/Orm/Orm.php');
-require_once(__DIR__.'/Orm/Orm_table.php');
-require_once(__DIR__.'/Orm/Orm_primary_key.php');
-require_once(__DIR__.'/Orm/Orm_field.php');
-require_once(__DIR__.'/Orm/Orm_association.php');
-require_once(__DIR__.'/Orm/Orm_validation.php');
-require_once(__DIR__.'/Orm/Orm_model.php');
+class Orm {
+
+    /**
+     * Instance de Codeigniter
+     * @var object $CI
+     */
+    protected static $CI = NULL;
+    
+    /**
+     * Version de l'ORM
+     * @var string 
+     */
+    protected $version = '3.2.11 (20141218)';
+
+    /**
+     * Configuration de l'ORM
+     * @var array 
+     */
+    protected static $config = array(
+        'cache' => FALSE,
+        'tts' => 3600,
+        'autoloadmodel' => FALSE,
+        'binary_enable' => FALSE, // MySQL 5.6 minimum
+        'encryption_enable' => FALSE, // MySQL 5.6 minimum
+        'encryption_key' => NULL // MySQL 5.6 minimum
+    );
+
+    /**
+     * Constructeur
+     * @param array $config
+     */
+    function __construct(array $config = array()) {
+        // Initialise la configuration, si elle existe
+        if (isset($config['orm']))
+            self::$config = array_merge(self::$config, $config['orm']);
+
+        // Premier chargement de L'ORM
+        if (self::$CI === NULL) {
+            // Charge l'instance de CodeIgniter
+            self::$CI = & get_instance();
+            
+            // Charge le fichier langue
+            self::$CI->load->language('orm');
+
+            // Si la clé de cryptage est vide, on désactive le cryptage
+            if (self::$config['encryption_enable'] && empty(self::$config['encryption_key']))
+                self::$config['encryption_enable'] = FALSE;
+
+            // Charge l'autoloader de L'ORM
+            if (self::$config['autoloadmodel'])
+                self::$CI->load->helper('orm');
+
+            // Si le cryptage est actif charge les éléments indispensable au cryptage
+            if (self::$config['encryption_enable']) {
+                self::$CI->load->helper('string');
+            }
+        }
+    }
+    
+    /**
+     * Retourne la version de l'ORM
+     * @return string
+     */
+    public function get_version() {
+        return $this->version;
+    }
+}
 
 /* End of file Orm.php */
 /* Location: ./application/libraries/Orm.php */
